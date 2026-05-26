d74519d34b7e2ae656930098fd8878f0f387be66
kuro-spm <sarapratsm@gmail.com>
2026-05-26 09:15:48 +0200

    film

--- models/models.py
+++ models/models.py
@@ -38,11 +38,36 @@ class CinemaPerson(models.Model):
             else:
                 obj.full_name = ''
 
-    
+    @api.constrains('birthdate, date_of_death')
+    def _check_birthdate_date_of_death(self):
+        for obj in self:
+            if obj.date_of_death and obj.birthdate:
+              if obj.date_of_death < obj.birthdate:
+                    raise ValidationError(_("Date of death cannot be earlier than the birth date."))
+      
 
 class CinemaFilm(models.Model):
     _name = 'cinema.film'
     _description = 'Cinema Film'
+
+    title = fields.Char('Title', size=60, required=True)
+    year = fields.Date('year')
+    duration = fields.Integer(string='Duration', help='Duration in minutes')    
+    
+    type = fields.Char(compute='_compute_type', string='type')
+    
+    @api.depends('duration')
+    def _compute_type(self):
+        for obj in self:
+            if obj.duration:
+                if obj.duration < 30: obj.type='short film'
+                elif obj.duration < 60: obj.type='medium-length film'
+                else: obj.type = 'full-length film'
+
+    synopsis = fields.Text('synopsis')
+    web_page = fields.Char('web page', size=60)
+    poster = fields.Binary('poster')
+
 
 
 
