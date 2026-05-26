47431c0ca051fe0388c06e270a9831ddee75ea1a
kuro-spm <sarapratsm@gmail.com>
2026-05-26 09:33:06 +0200

    autoinit

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 
-from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _ , tools
 from odoo.exceptions import ValidationError 
  
 class CinemaPerson(models.Model):
@@ -9,14 +9,15 @@ class CinemaPerson(models.Model):
 
     first_name = fields.Char('First Name', size=25, required=True)
     last_name = fields.Char('Last Name', size=45, required=True)
-    is_director = fields.Boolean('Is Director')
-    is_actor = fields.Boolean('Is Actor')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other','Other')],'Gender')
+    is_director = fields.Boolean('Is Director', required=True)
+    is_actor = fields.Boolean('Is Actor', required=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other','Other')],'Gender', required=True)
     birthdate = fields.Date('Birthdate', required=True)
     date_of_death = fields.Date('Date of Death')
 
     directed_films_ids = fields.One2many('cinema.film', 'director_id', string='Directed Films')
     acted_films_ids = fields.Many2many('cinema.film', string='Acted Films')
+    country_id = fields.Many2one('res.country','Citizenship', required=True)
 
     full_name = fields.Char(compute='_compute_full_name', string='Full name')
     
@@ -36,17 +37,33 @@ class CinemaPerson(models.Model):
                     raise ValidationError(_("Date of death cannot be earlier than the birth date."))
       
 
+    def _auto_init(self):
+        res = super(CinemaPerson, self)._auto_init()
+        # Passem cada columna de manera independent dins de la llista
+        tools.create_unique_index(
+            self._cr, 
+            'cinema_person_unique_name_lastname_birthdate',
+            self._table, 
+            ['first_name', 'last_name', 'birthdate']
+        )
+        return res
+
+
 class CinemaFilm(models.Model):
     _name = 'cinema.film'
     _description = 'Cinema Film'
 
     title = fields.Char('Title', size=60, required=True)
-    year = fields.Date('year')
+    year = fields.Date('year', required=True)
     duration = fields.Integer(string='Duration', help='Duration in minutes', required=True)    
     
     director_id = fields.Many2one('cinema.person', string='director')
     type = fields.Char(compute='_compute_type', string='type')
     
+    synopsis = fields.Text('synopsis')
+    web_page = fields.Char('web page', size=60)
+    poster = fields.Binary('poster')
+
     @api.depends('duration')
     def _compute_type(self):
         for obj in self:
@@ -57,11 +74,14 @@ class CinemaFilm(models.Model):
             else:
                 obj.type='unknown'
 
-    synopsis = fields.Text('synopsis')
-    web_page = fields.Char('web page', size=60)
-    poster = fields.Binary('poster')
+    @api.constrains('year')
+    def _constrains_year(self):
+        for obj in self:
+            if obj.duration:
+                if obj.year<1895:
+                    raise ValidationError(_('Year must be past 1895.'))
 
-
+   
 
 
     #campo_id = fields.Many2one('modelo.relacionado', string='Etiqueta') 
